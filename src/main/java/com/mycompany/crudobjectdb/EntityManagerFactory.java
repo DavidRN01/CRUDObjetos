cf126f4f7cb963e089cbb459d7559efacb450246
DavidRamosNavas <DavidRamosNavas@CS-EST-PC-13145>
2022-01-10 14:40:47 +0100

    static entity manager factory

---
+++ src/main/java/com/mycompany/crudobjectdb/EntityManagerFactory.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.crudobjectdb;
+
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author DavidRamosNavas
+ */
+public class EntityManagerFactory {
+    
+    private static javax.persistence.EntityManagerFactory emfp;
+    
+    static {
+        try {
+            emfp = Persistence.createEntityManagerFactory("pedidos.odb");
+        } catch(Exception ex) {
+            System.out.println("Error al iniciar EntityManagerFactory");
+        }
+    }
+    
+    private static javax.persistence.EntityManagerFactory emfc;
+    
+    static {
+        try {
+            emfc = Persistence.createEntityManagerFactory("carta.odb");
+        } catch(Exception ex) {
+            System.out.println("Error al iniciar EntityManagerFactory");
+        }
+    }
+    
+}
