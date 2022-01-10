a09df90b5f059a88c20b9a811ccc335b196bc32f
DavidRamosNavas <DavidRamosNavas@CS-EST-PC-13145>
2022-01-10 14:29:55 +0100

    Modelos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,10 @@ module com.mycompany.crudobjectdb {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.persistence;
+    requires lombok;
 
     opens com.mycompany.crudobjectdb to javafx.fxml;
+    opens models;
     exports com.mycompany.crudobjectdb;
 }
